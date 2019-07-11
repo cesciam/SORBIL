7ead2e6c5ec12152ffb4d7aad0758446c56e48a7
Alejandro <alesanc001@gmail.com>
2019-07-10 21:45:04 -0600

    Add: Capeta api con los archivos de nodejs
    Acordarse de darle npm install para bajar los node_modules.

---
+++ api/models/contacto.model.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+//Esquema del contacto
+
+let contacto_schema = new mongoose.Schema({
+    nombre: { type: String, required: true, unique: false },
+    correo: { type: String, required: true, unique: false },
+    nacimiento: { type: Date, required: true, unique: false },
+    comentario: { type: String, required: true, unique: false }
+});
+
+module.exports = mongoose.model('Contacto', contacto_schema);
