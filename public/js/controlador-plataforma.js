d7f174f5ae590976061624f91a4b751c2b5d46bd
Alejandro <alesanc001@gmail.com>
2019-07-12 02:15:49 -0600

    Add: sidebar funcional del admin
    TODO: dropdown del header y arreglar el height

--- public/js/controlador-plataforma.js
+++ public/js/controlador-plataforma.js
@@ -2,3 +2,18 @@
 
 //Javascript general de la plataforma
 
+// Función que permite mostrar y ocultar los links de listados y registros
+const click_menu = (click) => document.querySelector(click);
+
+//Dropdown de registros
+click_menu('.dropdown-toggle1').addEventListener('click', (fixed) => {
+    fixed.preventDefault();
+    click_menu('.dropdown-menu1').classList.toggle('dropdown-mostrar1');
+});
+
+//Dropdown de listados
+click_menu('.dropdown-toggle2').addEventListener('click', (fixed) => {
+    fixed.preventDefault();
+    click_menu('.dropdown-menu2').classList.toggle('dropdown-mostrar2');
+});
+
