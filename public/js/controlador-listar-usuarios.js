1b6d982df745b6c426ba61d7d44db750c152a222
Alejandro <alesanc001@gmail.com>
2019-07-19 16:06:44 -0600

    Revert "Add: avance de select y club de lectura"
    This reverts commit 4282869cd6fcc489330a61662198143f012a3ded.

--- public/js/controlador-listar-usuarios.js
+++ public/js/controlador-listar-usuarios.js
@@ -2,10 +2,10 @@
 
 const tbody = document.querySelector('#tabla-usuario tbody');
 let lista_usuarios = [];
-let txt_filtro = document.querySelector('#txt-filtro');
+let txt_filtro = document.querySelector('#txt_filtro');
 
 
-let mostrar_tabla = async () => {
+let mostrar_tabla = async() => {
 
     lista_usuarios = await obtenerUsuarios();
     tbody.innerHTML = '';
@@ -24,21 +24,21 @@ let mostrar_tabla = async () => {
     }
 };
 
-let filtrar_tabla = async () => {
+let filtrar_tabla = async() => {
 
     let filtro = txt_filtro.value.toLowerCase();
     tbody.innerHTML = '';
 
     for (let i = 0; i < lista_usuarios.length; i++) {
         if (lista_usuarios[i]['usuario'].toLowerCase().includes(filtro) ||
-            lista_usuarios[i]['nombre'].toLowerCase().includes(filtro) ||
-            lista_usuarios[i]['correo'].toLowerCase().includes(filtro) ||
-            lista_usuarios[i]['primer_apellido'].toLowerCase().includes(filtro) ||
-            lista_usuarios[i]['segundo_apellido'].toLowerCase().includes(filtro) ||
-            lista_usuarios[i]['provincia'].toLowerCase().includes(filtro) ||
-            lista_usuarios[i]['canton'].toLowerCase().includes(filtro) ||
-            lista_usuarios[i]['distrito'].toLowerCase().includes(filtro)) {
-
+         lista_usuarios[i]['nombre'].toLowerCase().includes(filtro) || 
+         lista_usuarios[i]['correo'].toLowerCase().includes(filtro) ||
+          lista_usuarios[i]['primer_apellido'].toLowerCase().includes(filtro) || 
+          lista_usuarios[i]['segundo_apellido'].toLowerCase().includes(filtro) ||
+          lista_usuarios[i]['provincia'].toLowerCase().includes(filtro) ||
+          lista_usuarios[i]['canton'].toLowerCase().includes(filtro) ||
+          lista_usuarios[i]['distrito'].toLowerCase().includes(filtro)  ) {
+              
             let fila = tbody.insertRow();
             fila.insertCell().innerHTML = lista_usuarios[i]['usuario'];
             fila.insertCell().innerHTML = lista_usuarios[i]['nombre'];
