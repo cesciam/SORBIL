9f5fe6e62eddeeff466080095796d860e61f00c2
Cristopher <csc8848@gmail.com>
2019-07-29 20:11:49 -0600

    fix: error html inicio de sesion

--- public/js/validar-usuario-adminLib.js
+++ public/js/validar-usuario-adminLib.js
@@ -4,7 +4,7 @@ let usuarioActivo = JSON.parse(sessionStorage.getItem('activo'));
 //let tipo_usuario = usuarioActivo.tipo_usuario;
 
 if(usuarioActivo == null){
-    window.location.href = 'u-iniciar-sesion.html';
+ //   window.location.href = 'u-iniciar-sesion.html';
 
 }
 
