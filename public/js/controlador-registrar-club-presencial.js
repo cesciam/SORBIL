ab815b49579bb54935f8680d6b35277bd81e6ccd
Alejandro <alesanc001@gmail.com>
2019-07-31 13:15:36 -0600

    Fixed: html titles

--- public/js/controlador-registrar-club-presencial.js
+++ public/js/controlador-registrar-club-presencial.js
@@ -164,6 +164,8 @@ let llamar = () => {
             title: 'Se ha registrado la información exitosamente',
             type: 'success',
         })
+        //Se llama a la función para limpiar el formulario
+        limpiarFormulario();
     } else {
         Swal.fire({ //formato json
             title: 'No se ha registrado la información',
@@ -171,6 +173,23 @@ let llamar = () => {
             text: 'Revisá los campos resaltados e intentalo de nuevo'
         })
     }
+};
+
+//Función para limpiar el formulario
+const limpiarFormulario = () => {
+    input_administrador_club.value = '';
+    input_tema.value = '';
+    input_administrador_club.value = '';
+    input_correo.value = '';
+    input_telefono.value = '';
+    input_categoria.value = '';
+    input_genero.value = '';
+    input_fecha.value = '';
+    input_provincia.value = '';
+    input_canton.value = '';
+    input_distrito.value = '';
+    input_direccion_exacta.value = '';
+    input_descripcion.value = '';
 };
 
 btn_enviar.addEventListener('click', llamar);
