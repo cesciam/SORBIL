79a73f017078a5eddd34e64ccf79c0158b1e1807
Fernanda <Granados>
2019-08-01 21:17:15 -1100

    Fixed: cambios en el perfil de usuario

--- public/js/controlador-listar-usuarios.js
+++ public/js/controlador-listar-usuarios.js
@@ -6,50 +6,53 @@ let txt_filtro = document.querySelector('#txt-filtro');
 const sct_usuarios = document.querySelector('#lista-usuarios');
 
 
-let mostrar_cards = async() => {
+let mostrar_cards = async () => {
 
     lista_usuarios = await obtenerUsuarios();
+
     for (let i = 0; i < lista_usuarios.length; i++) {
 
         let tipoUsuario = lista_usuarios[i]['tipo_usuario'];
+        if (tipoUsuario == 'u') {
 
-        let contenedor_card = document.createElement('div');
-        contenedor_card.classList.add('card');
+            let contenedor_card = document.createElement('div');
+            contenedor_card.classList.add('card');
 
-        let header = document.createElement('header');
-        let h2 = document.createElement('h2');
-        h2.innerText = lista_usuarios[i]['nombre'];
+            let header = document.createElement('header');
+            let h2 = document.createElement('h2');
+            h2.innerText = lista_usuarios[i]['nombre'];
 
-        header.appendChild(h2);
+            header.appendChild(h2);
 
-        let contenedor_imagen = document.createElement('div');
-        contenedor_imagen.classList.add('contenedor_imagen');
-        let foto = document.createElement('img');
-        foto.src = lista_usuarios[i]['avatar'];
+            let contenedor_imagen = document.createElement('div');
+            contenedor_imagen.classList.add('contenedor_imagen');
+            let foto = document.createElement('img');
+            foto.src = lista_usuarios[i]['avatar'];
 
-        contenedor_imagen.appendChild(foto);
+            contenedor_imagen.appendChild(foto);
 
-        let p_correo = document.createElement('p');
-        p_correo.innerText = lista_usuarios[i]['correo'];
+            let p_correo = document.createElement('p');
+            p_correo.innerText = lista_usuarios[i]['correo'];
 
-        let btn_perfil = document.createElement('button');
-        btn_perfil.innerText = 'Ver perfil';
-        btn_perfil.dataset._id = lista_usuarios[i]['correo'];
-        btn_perfil.addEventListener('click', function() {
-            //console.log(this.dataset._id);
-            window.location.href = `ver-perfil-usuario.html?_id=${this.dataset._id}`
-        });
+            let btn_perfil = document.createElement('button');
+            btn_perfil.innerText = 'Ver perfil';
+            btn_perfil.dataset._id = lista_usuarios[i]['correo'];
+            btn_perfil.addEventListener('click', function () {
+                //console.log(this.dataset._id);
+                window.location.href = `ver-perfil-usuario.html?_id=${this.dataset._id}`
+            });
 
-        contenedor_card.appendChild(header);
-        contenedor_card.appendChild(contenedor_imagen);
-        contenedor_card.appendChild(p_correo);
-        contenedor_card.appendChild(btn_perfil);
+            contenedor_card.appendChild(header);
+            contenedor_card.appendChild(contenedor_imagen);
+            contenedor_card.appendChild(p_correo);
+            contenedor_card.appendChild(btn_perfil);
 
-        sct_usuarios.appendChild(contenedor_card);
+            sct_usuarios.appendChild(contenedor_card);
+        }
     }
 };
 
-let filtrar_cards = async() => {
+let filtrar_cards = async () => {
 
     let filtro = txt_filtro.value.toLowerCase();
     sct_usuarios.innerHTML = '';
@@ -68,7 +71,7 @@ let filtrar_cards = async() => {
             let contenedor_imagen = document.createElement('div');
             contenedor_imagen.classList.add('contenedor_imagen');
             let foto = document.createElement('img');
-            foto.src = '../imgs/avatar-placeholder.png';
+            foto.src = lista_usuarios[i]['avatar'];
 
             contenedor_imagen.appendChild(foto);
 
@@ -78,7 +81,7 @@ let filtrar_cards = async() => {
             let btn_perfil = document.createElement('button');
             btn_perfil.innerText = 'Ver perfil';
             btn_perfil.dataset._id = lista_usuarios[i]['_id'];
-            btn_perfil.addEventListener('click', function() {
+            btn_perfil.addEventListener('click', function () {
                 //console.log(this.dataset._id);
                 window.location.href = `ver-perfil-usuario.html?_id=${this.dataset._id}`
             });
