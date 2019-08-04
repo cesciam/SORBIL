be362bd93bb13fc13030fdf6796ede6b638232f4
Cristopher <csc8848@gmail.com>
2019-08-03 19:49:10 -0600

    ADD: lISTADO DE AUTORES AL REGISTRO DEL LIBRO

--- public/js/validar-usuario-adminLib.js
+++ public/js/validar-usuario-adminLib.js
@@ -1,15 +1,25 @@
 'use strict';
 
-let usuarioActivo = JSON.parse(sessionStorage.getItem('activo'));
-//let tipo_usuario = usuarioActivo.tipo_usuario;
+let usuarioActivo = false;
+
+usuarioActivo = JSON.parse(sessionStorage.getItem('activo'));
 
 if(usuarioActivo == null){
     window.location.href = 'u-iniciar-sesion.html';
 }
 
-if(usuarioActivo != 'al'){
-    window.location.href = '../index.html';
+if(usuarioActivo){
+    let tipo_usuario = usuarioActivo.tipo_usuario;
+
+    if(tipo_usuario != 'al'){
+        window.location.href = '../index.html';
+    }
 }
+
+
+
+
+
 
 
 
