193749ee06ae08f8340b6d044fe1fcebf902ccaa
Alejandro <alesanc001@gmail.com>
2019-08-04 14:32:12 -0600

    Add: al-registrar perfil

--- public/js/controlador-inicio-al.js
+++ public/js/controlador-inicio-al.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const btn_perfil = document.querySelector('#ver_perfil_admin_libreria');
-const btn_perfil_libro = document.querySelector('#ver_perfil_admin_libro');
-const btn_perfil_categoria = document.querySelector('#ver_perfil_admin_categoria');
+const btn_registrar_categoria = document.querySelector('#registrar_adminal_categoria');
+const btn_registrar_genero = document.querySelector('#registrar_adminal_genero');
+const btn_registrar_club = document.querySelector('#registrar_adminal_club');
+const btn_registrar_oferta = document.querySelector('#registrar_adminal_oferta');
 
 usuarioActivo = JSON.parse(sessionStorage.getItem('activo'));
 let _id = usuarioActivo._id;
@@ -11,10 +13,18 @@ btn_perfil.addEventListener('click', function () {
     window.location.href = `ver-perfil-administrador-libreria.html?_id=${_id}`
 });
 
-btn_perfil_categoria.addEventListener('click', function () {
+btn_registrar_categoria.addEventListener('click', function () {
     window.location.href = `al-registrar-categorias.html?_id=${_id}`
 });
 
-btn_perfil_categoria.addEventListener('click', function () {
-    window.location.href = `al-registrar-categorias.html?_id=${_id}`
+btn_registrar_genero.addEventListener('click', function () {
+    window.location.href = `al-registrar-generos.html?_id=${_id}`
+});
+
+btn_registrar_club.addEventListener('click', function () {
+    window.location.href = `al-seleccionar-club.html?_id=${_id}`
+});
+
+btn_registrar_oferta.addEventListener('click', function () {
+    window.location.href = `al-registrar-oferta.html?_id=${_id}`
 });
