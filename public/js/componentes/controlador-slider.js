89ca5b231c93c77e2893923a6f3b24af6311fcef
Alejandro <alesanc001@gmail.com>
2019-08-05 12:48:30 -0600

    Add: cambios diseño

--- public/js/componentes/controlador-slider.js
+++ public/js/componentes/controlador-slider.js
@@ -2,7 +2,7 @@
 //Slider
 let glide = new Glide('.glide', {
     type: 'carrousel', //hay dos tipos slider y carrousel
-    startAt: 8, //a dónde inicia el slider
+    startAt: 5, //a dónde inicia el slider
     peek: 1, //tamaño de los elementos
     gap: 5, //espacio entre los elementos
     bound: false, // que no llegue hasta el final
@@ -12,6 +12,7 @@ let glide = new Glide('.glide', {
     autoplay: 3000, //tiempo del slide en miliseg
     perView: 8, //cantidad de slide en pantalla
     breakpoints: { //para otros dispositivos
+        1366: { perView: 5 }, //elementos para tablet
         800: { perView: 2 }, //elementos para tablet
         480: { perView: 1 } // elementos para mobile
     }
