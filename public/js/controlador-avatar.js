1426e448cf655277d5cb96975a75c71b1e20068f
Cristopher <csc8848@gmail.com>
2019-08-04 22:00:51 -0600

    FIXED

--- public/js/controlador-avatar.js
+++ public/js/controlador-avatar.js
@@ -2,5 +2,12 @@ const avatar = document.querySelector('#avatar-img');
 
 let UsuarioEnSesion = JSON.parse(sessionStorage.getItem('activo'));
 
-
 avatar.src = UsuarioEnSesion.avatar;
+
+let id_ensesion = UsuarioEnSesion._id;
+let btn_perfil = document.querySelector('#ver_perfil_usuario');
+
+
+btn_perfil.addEventListener('click', function(){
+    window.location.href = `./views/ver-perfil-usuario.html?_id=${id_ensesion}`;
+});
