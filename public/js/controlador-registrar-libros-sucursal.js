9bc0eac7ee79185137785cf806fe2a4703a58a14
Cristopher <csc8848@gmail.com>
2019-08-19 01:17:33 -0600

    FIXED: REGISTRO DE LIBROS EN LIBRERIA CON VALIDACIONES

--- public/js/controlador-registrar-libros-sucursal.js
+++ public/js/controlador-registrar-libros-sucursal.js
@@ -6,7 +6,7 @@ let btn_registrar = document.querySelector('#btn-registrar');
 let cantidad_disponible = document.querySelector('#cantidad_libros_disponible');
 let libreria = [];
 let correoLibrosSucursal;
-
+let idSuc;
 
 const urlParams = new URLSearchParams(window.location.search);
 let id = urlParams.get('_id');
@@ -35,6 +35,17 @@ let agregar_sucursales = async () => {
     
 };
 
+let posicionSucursal=(psucursal)=>{
+    let posicion;
+    for (let i = 0; i < libreria[0].sucursales.length; i++) {
+        if(psucursal == libreria[0].sucursales[i].nombre)
+        posicion = i;
+        
+    }
+    return posicion;
+}
+
+
 let validar = (pselect, pcantidad)=>{
     let error = false;
 
@@ -44,8 +55,9 @@ let validar = (pselect, pcantidad)=>{
     } else {
         selectSucursales.classList.remove('input_error');
     }
-
-    let cantidadparavalidar = parseInt(libreria[0].libros[i].cantidad);
+    pcantidad = parseInt(pcantidad);
+    let posicionLib = posicionLibro();
+    let cantidadparavalidar = parseInt(libreria[0].libros[posicionLib].cantidad);
     if(pcantidad > cantidadparavalidar || pcantidad == '' || pcantidad <= 0){
         error = true;
         cantidad.classList.add('input_error');
@@ -66,55 +78,78 @@ let obteneridsuc =(psucursal)=>{
     return id;
 }
 
-let posicionSucursal=(psucursal)=>{
+
+
+let validarSiYaExisteLibro = (pcantidadIngresada) =>{
+    let existe = false;
+    let posicionLib = posicionLibro();
+    idSuc = obteneridsuc(selectSucursales.value);
+    for (let i = 0; i < libreria[0].librosSuc.length; i++) {
+       if(libreria[0].librosSuc[i].idlibro == id && libreria[0].librosSuc[i].idSuc == idSuc){
+        existe = true;
+        let num1 = parseInt(libreria[0].librosSuc[i].cantidad);
+        let num2 = parseInt(pcantidadIngresada);
+        let suma = num1 + num2;
+        let sumString = suma.toString(); 
+        libreria[0].librosSuc[i].cantidad = sumString;
+        // Resta de libros en Libreria
+        let CantidadTotalLibroLibreria = libreria[0].libros[posicionLib].cantidad;
+        let res1 = parseInt(CantidadTotalLibroLibreria);
+        let resta = res1 - num2;
+        let reString = resta.toString();
+        libreria[0].libros[posicionLib].cantidad = reString;
+
+        existe = true;
+        }
+    }
+    return existe;
+};
+
+let restarLibros =(pcantidad)=>{
+    let posicionLib = posicionLibro();
+    let CantidadTotalLibroLibreria = libreria[0].libros[posicionLib].cantidad;
+    let res1 = parseInt(CantidadTotalLibroLibreria);
+    let res2 = parseInt(pcantidad);
+    let resta = res1 - res2;
+    let reString = resta.toString();
+    libreria[0].libros[posicionLib].cantidad = reString;
+}
+
+let posicionLibro= ()=>{
     let posicion;
-    for (let i = 0; i < libreria[0].sucursales.length; i++) {
-        if(psucursal == libreria[0].sucursales[i].nombre)
-        posicion = i;
-        
+    for (let i = 0; i < libreria[0].libros.length; i++) {
+        if(id == libreria[0].libros[i].idlibro){
+            posicion = i;
+        }
     }
     return posicion;
 }
-
-let validarSiYaExisteLibro = (pid, libreria) =>{
-    let existe = false;
-    let posicionlib;
-    let posicionSuc= posicionSucursal(selectSucursales.value);
-    
-
-    for(let i = 0; i < libreria[0].sucursales[posicionSuc].librosSuc.length; i++){
-            if (pid == libreria[0].sucursales[posicionSuc].librosSuc[i].id){
-                existe = true;
-                posicionlib = i;
-            }   
-           
-    }
-
-    if(existe == false){
-        return false;
-    }else{
-        return posicionlib;
-    }
-
-};
 
 let llamar =()=>{
 let error = validar(selectSucursales.value, cantidad.value);
 let idSuc = obteneridsuc(selectSucursales.value);
 if (!error){
-    registrarLibrosSuc(id, idSuc, cantidad.value, correoLibrosSucursal);
+    let existe = validarSiYaExisteLibro(cantidad.value);
+    if (existe){
+        
+        modificarSucursal(correoLibrosSucursal ,libreria[0]);
+    }else{
+        restarLibros(cantidad.value);
+        registrarLibrosSuc(id, idSuc, cantidad.value, correoLibrosSucursal);
+        modificarSucursal(correoLibrosSucursal ,libreria[0]);
+    }
+
+    Swal.fire({ //formato json
+        title: 'Se ha registrado la información exitosamente',
+        type: 'success',
+    }).then((result) => {
+        if (result.value) {
+            window.location.href = `al-listar-libros-liberia.html`;
+        }
+    })
 }
 
 
-// if(!error){
-//     let existe = validarSiYaExisteLibro(id, libreria);
-//     if( typeof(existe)== 'number'){
-//         console.log('Existe');
-        
-//     }else{
-//         console.log('No existe');
-
-//     }
 
 
 }
