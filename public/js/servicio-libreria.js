4ebaac6c52ce8a2cbc3215c1d86d33941783d285
Cristopher <csc8848@gmail.com>
2019-08-23 12:49:09 -0600

    Fix: listar librerias

--- public/js/servicio-libreria.js
+++ public/js/servicio-libreria.js
@@ -210,17 +210,7 @@ let deshabilitarLibreria = (pid) => {
     });
 };
 
-let eliminarLibreria = (pid) => {
-    axios({
-        method: 'post',
-        url: 'http://localhost:4000/api/eliminar-libreria',
-        responseType: 'json',
-        data: {
-            _id: pid
-        }
-    });
 
-}
 
 let modificarLibreria = (pidcorreo, pimagen, pusuario, pcorreo, pempresa, ptelefono, pdescripcion, pprovincia, pcanton, pdistrito, pdireccion_exacta, pdireccion_latitud, pdireccion_longitud) => {
     axios({
