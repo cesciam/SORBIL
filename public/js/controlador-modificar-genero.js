7ed3287bf523124bf69d06e93000f733ef1a28e4
Alejandro <alesanc001@gmail.com>
2019-08-24 06:46:18 -0600

    Fixed: pulgas

--- public/js/controlador-modificar-genero.js
+++ public/js/controlador-modificar-genero.js
@@ -38,26 +38,33 @@ let editar = () => {
 
     if (!error) {
     modificarGenero(_id, genero);
-    switch (tipoUserMod) {
-        case 'al': {
-            window.location.href = `al-listar-generos.html?_id=${_id}`;
-            break;
-        }
-
-        case 'ap': {
-            window.location.href = `ap-listar-generos.html?_id=${_id}`;
-            break;
+        Swal.fire({ //formato json
+            title: 'Se ha modificado la información exitosamente',
+            type: 'success',
+        }).then((result) => {
+            if (result.value) {
+                switch (tipoUserMod) {
+                    case 'al': {
+                        window.location.href = `al-listar-generos.html?_id=${_id}`;
+                        break;
+                    }
+                    case 'ap': {
+                        window.location.href = `ap-listar-generos.html?_id=${_id}`;
+                        break;
+                    }
+                }
             }
-        }
+        });
+        //Se llama a la función para limpiar el formulario
 
     } else {
         Swal.fire({ //formato json
-            title: 'No se ha registrado la información',
+            title: 'No se ha modificado la información',
             type: 'warning',
             text: 'Revise los campos resaltados e inténtelo de nuevo'
         })
     }
-}
-
+};
+    
 cargar_formulario();
 boton_enviar.addEventListener('click', editar);
